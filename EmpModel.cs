a7fc1e4e78cad9ee0adba6894e9f75f1353bd30b
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-11-11 11:07:13 +0530

    Employee Model

---
+++ EmpModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollService
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public char Gender { get; set; }
+        public decimal BasicPay { get; set; }
+        public decimal Deduction { get; set; }
+        public decimal TaxablePay { get; set; }
+        public decimal Tax { get; set; }
+        public decimal NetPay { get; set; }
+        public DateTime startDate { get; set; }
+
+    }
+}
+
